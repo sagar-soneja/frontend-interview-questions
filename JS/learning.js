796aa6ea640044ce581a5d410999ddea1f1c08fe
Sagar Soneja <78483562+sagar-soneja@users.noreply.github.com>
2023-09-06 13:12:19 +0530

    Add files via upload

--- JS/learning.js
+++ JS/learning.js
@@ -50,47 +50,47 @@
   
 // infinite curring :-
 
-function add(a) {
-    return function(b) {
-        if(b) return add(a+b);
-        return a;
-    };
-}
+// function add(a) {
+//     return function(b) {
+//         if(b) return add(a+b);
+//         return a;
+//     };
+// }
 
-console.log(add(5)(3)(2)(6)())
+// console.log(add(5)(3)(2)(6)())
 
 //partial function transform one function into another function with small arity
 // arity means no of function or arguments function receive
 
-function sum(a) {
-    return function(b,c) {
-        return a+b+c
-    }
-}
+// function sum(a) {
+//     return function(b,c) {
+//         return a+b+c
+//     }
+// }
 
-const x = sum(10)
-console.log(x(5,6))
-console.log(x(3,2))
+// const x = sum(10)
+// console.log(x(5,6))
+// console.log(x(3,2))
 
 // curring custom , making function that takes normal function and change it into curry
-function curry(func) {
-    return function curriedFunc(...args) {
-        if(args.length >= func.length) {
-            return func(...args);
-        } else {
-            return function (...next) {
-                return curriedFunc(...args, ...next);
-            }
-        }
-    }
-}
+// function curry(func) {
+//     return function curriedFunc(...args) {
+//         if(args.length >= func.length) {
+//             return func(...args);
+//         } else {
+//             return function (...next) {
+//                 return curriedFunc(...args, ...next);
+//             }
+//         }
+//     }
+// }
 
 
-const sum = (a,b,c,d) => a + b + c +d
+// const sum = (a,b,c,d) => a + b + c +d
 
-const totalsum = curry(sum);
+// const totalsum = curry(sum);
 
-console.log(totalsum(1)(6)(5)(6))
+// console.log(totalsum(1)(6)(5)(6))
 // ========================================================================================
 
 // Hoisting :- It is behavior in JS where variable and function declaration are moved to the
@@ -143,7 +143,8 @@ console.log(totalsum(1)(6)(5)(6))
 // call:
 
 // call is a method available on all JavaScript functions.
-// It invokes a function immediately, allowing you to specify both the value of this and the arguments the function should receive.
+// It invokes a function immediately, allowing you to specify both the value of this and 
+// the arguments the function should receive.
 // Arguments are passed to the function individually, separated by commas.
 // It does not create a new function but executes the function on the spot.
 
@@ -159,7 +160,8 @@ console.log(totalsum(1)(6)(5)(6))
 // apply:
 
 // apply is similar to call in that it invokes a function immediately and lets you specify the value of this.
-// However, it takes an array or an array-like object as its second argument, where the array elements are passed as individual arguments to the function.
+// However, it takes an array or an array-like object as its second argument, where the array elements are 
+// passed as individual arguments to the function.
 
 // function greet(name) {
 //     console.log(`Hello, ${name}! I am ${this.title}.`);
@@ -172,7 +174,8 @@ console.log(totalsum(1)(6)(5)(6))
 // bind:
 
 // bind is used to create a new function with a specific this value, without executing the function immediately.
-// It returns a new function that you can call later, and any arguments passed to the bound function will be prepended to the arguments passed when the bound function is invoked.
+// It returns a new function that you can call later, and any arguments passed to the bound function will
+// be prepended to the arguments passed when the bound function is invoked.
 // It is often used when you want to preserve a particular context for a function.  
 
 
@@ -214,17 +217,63 @@ console.log(totalsum(1)(6)(5)(6))
 // a series of functions in a right-to-left (or inner-to-outer) order. The result of one function 
 // becomes the input for the next function in the composition.
 
-// const compose = (...functions) => (input) =>
-//   functions.reduceRight((result, func) => func(result), input);
+const compose = (...functions) => (input) =>
+  functions.reduceRight((result, func) => func(result), input);
 
-// // Example functions to be composed
-// const add = (a) => (b) => a + b;
-// const multiply = (a) => (b) => a * b;
-// const subtract = (a) => (b) => a - b;
+// Example functions to be composed
+const add = (a) => (b) => a + b;
+const multiply = (a) => (b) => a * b;
+const subtract = (a) => (b) => a - b;
 
-// // Create a composed function
-// const calculate = compose(subtract(3), multiply(2), add(5));
+// Create a composed function
+const calculate = compose(subtract(3), multiply(2), add(5));
 
-// // Use the composed function
-// const result = calculate(10); // (((10 + 5) * 2) - 3) = 19
-// console.log(result); // Outputs 19
+// Use the composed function
+const result = calculate(10); // (((10 + 5) * 2) - 3) = 19
+console.log(result); // Outputs 19
+// console.log(Math.abs(result))
+
+
+// ============================================4
+
+// Redux :-
+// Redux is an open-source JavaScript library commonly used in web development, 
+// particularly with frameworks like React, to manage the state of an application. 
+// It provides a predictable state container for managing the data and the flow of data 
+// within a JavaScript application.
+
+
+// Store: Redux centralizes the application's state in a JavaScript object called a "store." The store holds all the data that represents the application's state.
+
+// Actions: Actions are plain JavaScript objects that describe what should happen in the application. They typically have a type property that specifies the action type and can include additional data.
+
+// Reducers: Reducers are pure functions that specify how the application's state should change in response to actions. They take the current state and an action as input and return a new state. Redux combines all reducers to produce a new state tree when actions are dispatched.
+
+// Dispatch: To update the state, you dispatch actions to the Redux store using the store.dispatch(action) method. The store then sends the action to the reducers, which determine how the state should be updated.
+
+// =========================
+
+// Temporal dead zone :- it is a state in which variables are being declared in scope but not initialized
+
+//flat array :-
+
+let arr = [
+    [1,2],
+    [3,4],
+    [5,6,[7,8]],
+    [9,10,11]
+]
+
+function customFlat(arr, depth = 1) {
+    let result = [];
+
+    arr.forEach((ar) => {
+        if(Array.isArray(ar) && depth > 0) {
+            result.push(...customFlat(ar,depth-1))
+        } else result.push(ar);
+    })
+    return result;
+
+    
+}
+console.log(customFlat(arr,2))
